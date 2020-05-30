732c25c101c16408f53e1df28a25e4729a905610
mbuccat <mbuccat@bu.edu>
2020-05-30 15:46:00 -0400

    Updates

---
+++ precache-manifest.ca4e2258b59a2c2f256deabd0881515e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8b1102d618dd65521ca7118f36d616",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "19b0efa3bcd507e8d20d",
+    "url": "/minesweeper/static/css/main.aa0108a1.chunk.css"
+  },
+  {
+    "revision": "e55f29590a6dae460cb8",
+    "url": "/minesweeper/static/js/2.465ae9b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.465ae9b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b0efa3bcd507e8d20d",
+    "url": "/minesweeper/static/js/main.4059c888.chunk.js"
+  },
+  {
+    "revision": "75c732f2aae1e0a1c0a7",
+    "url": "/minesweeper/static/js/runtime-main.a24c26e6.js"
+  }
+]);
